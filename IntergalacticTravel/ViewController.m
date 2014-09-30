fc542001fa9f65d5b376793a5f2efed7179de1f3
Eduardo Alvarado Díaz <GLB-MXM0001@Eduardos-MacBook-Pro.local>
2014-09-30 11:00:49 -0500

    Initial Commit

---
+++ IntergalacticTravel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IntergalacticTravel
+//
+//  Created by Eduardo Alvarado Díaz on 9/30/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
